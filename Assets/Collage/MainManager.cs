2f7b3e5358ebf6a085f12a6d90539dbe4ec9cdd4
Sassar <ta1.sakura96@gmail.com>
2014-10-17 11:53:28 +0900

    タイトルと本編の追加
    ・タイトル画面(Title)の追加
    ・本編(Main)の追加
    ・Title-Main間のシーン遷移機能の実�

---
+++ Assets/Collage/MainManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainManager : MonoBehaviour {
+
+	public float screenW;
+	public float screenH;
+
+	// Use this for initialization
+	void Start () {
+		screenW = Screen.width;
+		screenH = Screen.height;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Application.platform == RuntimePlatform.Android && Input.GetKey(KeyCode.Escape)) {
+			Application.LoadLevel(0);
+		}
+	}
+	void OnGUI(){
+		//TitleButton 
+		if (GUI.Button (new Rect (screenW-screenW*2/10-10, 10, 50, 25), "Title")) {
+			Application.LoadLevel(0);
+		}
+		//QuitButton
+		if (GUI.Button (new Rect (screenW-screenW/10-5, 10, 50, 25), "Quit")) {
+			Application.Quit ();
+		}
+	}
+}
