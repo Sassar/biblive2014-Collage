5bf0b450f1b9346b0d6e84ebdfb8a6dd1b57f7bd
Sassar <ta1.sakura96@gmail.com>
2014-10-22 03:23:05 +0900

    HeatMap用のボタンを1つ設置
    ヒートマップで変化する色の部分を表現するためのボタンを設置

--- Assets/Collage/HeatMap/HeatMap.cs
+++ Assets/Collage/HeatMap/HeatMap.cs
@@ -2,14 +2,17 @@
 using System.Collections;
 
 public class HeatMap : MonoBehaviour {
-
+	
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	}
+
+	void OnGUI(){
+		GUI.backgroundColor = Color.yellow;
+		GUI.Button(new Rect(10, 10, 70, 30), "A button");
 	}
 }
