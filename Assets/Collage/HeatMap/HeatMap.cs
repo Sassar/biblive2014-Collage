bfb6522a032a5f2b30537e49474af386f2445ca9
Sassar <ta1.sakura96@gmail.com>
2014-10-22 18:12:03 +0900

    HeatMapの棚の追加(再)
    消してしまったHeatMapの棚オブジェクト(とそのMaterial)の追加と、一部Heatmapスクリプトの追加

--- Assets/Collage/HeatMap/HeatMap.cs
+++ Assets/Collage/HeatMap/HeatMap.cs
@@ -2,9 +2,13 @@
 using System.Collections;
 
 public class HeatMap : MonoBehaviour {
-	
+
+	private GameObject A1;
+	//public string shelfs[50] ={A1, A3, A4, A5};
+
 	// Use this for initialization
 	void Start () {
+		A1 = GameObject.Find ("A1");
 	}
 	
 	// Update is called once per frame
