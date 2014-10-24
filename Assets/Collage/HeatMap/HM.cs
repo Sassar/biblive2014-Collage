1b6e7644af5e2e353e88649738b1194213b0b5bf
Sassar <ta1.sakura96@gmail.com>
2014-10-24 17:31:04 +0900

    動画の長さの取得を実�
    MovieTextureにアタッチされている動画の長さ取得を実�

---
+++ Assets/Collage/HeatMap/HM.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HM: MonoBehaviour {
+
+	private float alpha = 1.0f;
+	private float G = 0.0f;
+	private float B = 1.0f;
+
+	// This shelf name
+	private string Shelf_Name;
+	private MovieTexture mov_Texture;
+	GameObject Main_Shelf;
+
+	Color color_A3;
+//	 scriptManager;
+
+	// Use this for initialization
+	void Start () {
+		Shelf_Name = this.gameObject.name;
+		Shelf_Name = Shelf_Name.Replace("_HM", "");
+		Main_Shelf = GameObject.Find (Shelf_Name);
+
+		mov_Texture = Main_Shelf.renderer.material.mainTexture as MovieTexture;
+//		G = mov_Texture.duration % ;
+
+//		scriptManager = GetComponent("managerrrrrrr");
+//		game_OBJECT.renderer.material.color = new Color (1.0f, G, B, alpha);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Collage/HeatMap/HeatMap.cs
+++ Assets/Collage/HeatMap/HeatMap.cs
@@ -3,20 +3,26 @@ using System.Collections;
 
 public class HeatMap : MonoBehaviour {
 
-	private GameObject A1;
-	//public string shelfs[50] ={A1, A3, A4, A5};
+	private GameObject A3;
+	//public string shelfs[50] ={A3, A4, A5};
+	private float alpha = 1.0f;
+	private float G_A3 = 0.0f;
+	private float B_A3 = 1.0f;
+	Color color_A3;
+	string movieName_A3;
+	MovieTexture mov_A3;
 
 	// Use this for initialization
 	void Start () {
-		A1 = GameObject.Find ("A1");
+		// mov_A3 = Assets/Resources/A03/.renderer.materials.mainTexture;
+		movieName_A3 = "b1012045";
+
+		//G_A3 = .duaration % hogeeee;
+		A3 = GameObject.Find ("A3");
+		A3.renderer.material.color = new Color (1.0f, G_A3, B_A3, alpha);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	}
-
-	void OnGUI(){
-		GUI.backgroundColor = Color.yellow;
-		GUI.Button(new Rect(10, 10, 70, 30), "A button");
 	}
 }
